46d17dba2dd4b86ed211f913cd34af5ebd7b5adb
bkjoap <briana.kjoa.payne@gmail.com>
2023-04-01 15:05:51 -0500

    Sydney to show correct date and time

--- src/index.js
+++ src/index.js
@@ -7,8 +7,21 @@ let losAngelesCurrentDate = moment()
   .tz('America/Los_Angeles')
   .format('MMMM D, YYYY')
 
-let losAngelesCurrentTime = moment().tz('America/Los_Angeles').format('h:m')
+let losAngelesCurrentTime = moment().tz('America/Los_Angeles').format('hh:mm')
 let losAngelesCurrentMeridiem = moment().tz('America/Los_Angeles').format('A')
 
 losAngelesTimeElement.innerHTML = `${losAngelesCurrentTime} <span class="small">${losAngelesCurrentMeridiem}</span>`
 losAngelesDateElement.innerHTML = `${losAngelesCurrentDate}`
+
+//Sydney elements from moment
+let sydneyElement = document.querySelector('#sydney')
+let sydneyDateElement = document.querySelector('#sydney .date')
+let sydneyTimeElement = document.querySelector('#sydney .time')
+
+let sydneyCurrentDate = moment().tz('Australia/Sydney').format('MMMM D, YYYY')
+
+let sydneyCurrentTime = moment().tz('Australia/Sydney').format('hh:mm')
+let sydneyCurrentMeridiem = moment().tz('Australia/Sydney').format('A')
+
+sydneyTimeElement.innerHTML = `${sydneyCurrentTime} <span class="small">${sydneyCurrentMeridiem}</span>`
+sydneyDateElement.innerHTML = `${sydneyCurrentDate}`
